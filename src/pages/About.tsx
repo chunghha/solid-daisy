95029d505ade90d2a7b95b9066a9772e638ce237
Chris Ha <chunghha@users.noreply.github.com>
2022-10-02 05:40:14 -0500

    feat: example solid-primitives with Breakpoint

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,13 @@
 import { Component } from "solid-js"
+import Breakpoint from "../components/Breakpoint";
 
 const About: Component = () => {
   return (
     <div class="min-h-screen">
       <p class="text-inter p-8 text-center">This is a Solid demo that uses the Country API.</p>
+      <div class="mt-4">
+        <Breakpoint />
+      </div>
     </div>
   )
 }

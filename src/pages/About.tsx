8c7a889657797cd5001ec6661eba03e414da0861
Chris Ha <chunghha@users.noreply.github.com>
2022-11-07 06:36:00 -0600

    chore: display app version

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,6 @@
 import type { Component } from 'solid-js'
 import Breakpoint from '../components/Breakpoint'
+import Version from '../components/Version'
 
 const About: Component = () => {
   return (
@@ -7,6 +8,9 @@ const About: Component = () => {
       <p class="text-inter p-8 text-center">This is a Solid demo that uses the Country API.</p>
       <div class="mt-4">
         <Breakpoint />
+      </div>
+      <div class="flex items-center justify-center">
+        <Version />
       </div>
     </div>
   )

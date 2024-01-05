fbf3b763bc5aebd042468d5d467f9fc6592751a0
Chris Ha <chunghha@users.noreply.github.com>
2024-01-04 22:43:19 -0600

    build(lib): @solidjs/router v0.10.x

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
-import { Route, Routes } from '@solidjs/router'
 import type { Component } from 'solid-js'
 
-import Nav from './components/Nav'
-import About from './pages/About'
-import Ach from './pages/Ach'
-import Countries from './pages/Countries'
 import Home from './pages/Home'
-import { theme } from './stores/theme.store'
 
 const App: Component = () => {
   return (
-    <div data-theme={theme.isDark ? 'rosepine' : 'dawn'}>
-      <div class="bg-gradient-to-r from-base-100 to-neutral">
-        <div class="max-w-8xl mx-auto pt-4 pr-8 pb-8 pl-8">
-          <Nav />
-          <Routes>
-            <Route path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/country" component={Countries} />
-            <Route path="/ach" component={Ach} />
-          </Routes>
-        </div>
-      </div>
-    </div>
-
+    <Home />
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,32 @@
 import '@fontsource/fira-mono/index.css'
 import '@fontsource/inter/index.css'
 import '@fontsource/poppins/index.css'
+import { Route, Router } from '@solidjs/router'
 import { render } from 'solid-js/web'
-import { Router } from '@solidjs/router'
 
-import './index.css'
 import App from './App'
+import Nav from './components/Nav'
+import './index.css'
+import About from './pages/About'
+import Ach from './pages/Ach'
+import Countries from './pages/Countries'
+import { theme } from './stores/theme.store'
 
 render(
   () => (
-    <Router>
-      <App />
-    </Router>
+    <div data-theme={theme.isDark ? 'rosepine' : 'dawn'}>
+      <div class="bg-gradient-to-r from-base-100 to-neutral">
+        <div class="max-w-8xl mx-auto pt-4 pr-8 pb-8 pl-8">
+          <Nav />
+          <Router>
+            <Route path="/" component={App} />
+            <Route path="/about" component={About} />
+            <Route path="/country" component={Countries} />
+            <Route path="/ach" component={Ach} />
+          </Router>
+        </div>
+      </div>
+    </div>
   ),
   document.getElementById('root') as HTMLElement,
 )
